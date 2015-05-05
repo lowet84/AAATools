f37b7b220f383c1698785ceb1dbb1488685d873f
Fredrik Löwenhamn <lowet84@gmail.com>
2015-05-05 17:25:15 +0200

    suck...

--- app.js
+++ app.js
@@ -1,78 +1,22 @@
 //noinspection SpellCheckingInspection
 var Enumerable = require('linq');
 
-var attacker = {inf: 6, art: 3};
-var defender = {inf: 10};
+var attacker = {inf: 6, art: 3, fig:1, tac:1,bom:1};
+var defender = {inf: 10, aa: 2};
 
-battleRound(attacker, defender);
+var x = 0;
+
+landBattleRound(attacker, defender);
 
 
-function battleRound(attacker, defender) {
-    var units = require('./units');
+function landBattleRound(attacker, defender) {
+    var aaDies = Enumerable.from([defender.aa * 3,
+        (attacker.fig ? attacker.fig : 0) + (attacker.tac ? attacker.tac : 0) + (attacker.bom ? attacker.bom : 0)]).min()
 
-    var testConditions = [];
-    var priority = getPriority(units[2], testConditions);
-    addConditions(units[1], testConditions);
-    addConditions(units[1], testConditions);
-    addConditions(units[1], testConditions);
-    var x = 0;
-    x++;
-}
 
-function getPriority(unit, conditions) {
-    if (unit.priority == undefined) {
-        return {priority: 0, conditions: conditions}
-    }
-    for (i = 0; i < unit.priority.length; i++) {
-        var priority = unit.priority[i];
-        if (priority.condition == undefined) {
-            return priority.value;
-        }
 
-        var condition = Enumerable.from(conditions)
-            .firstOrDefault(function (d) {
-                return d.name == priority.condition
-            });
+    var attackDies = [0,0,0,0];
+    var defenseDies = [0,0,0,0];
 
-        if (priority.invertCondition == true) {
-            if (condition == undefined || condition.value == 0) {
-                return priority.value;
-            }
-        }
-        else if (condition.name == priority.condition) {
-            if(condition.value!=0){
-                condition.value--;
-                return priority.value;
-            }
-        }
-    }
-}
 
-function addConditions(unit,conditions, type){
-    for(i=0;i<unit.conditions.length;i++){
-        var condition = unit.conditions[i];
-
-        if(condition.type != type){
-            continue;
-        }
-
-        var existing = Enumerable.from(conditions)
-            .firstOrDefault(function (d) {
-                return d.name == condition.name
-            });
-        if(existing == undefined){
-            if(condition.value==undefined){
-                condition.value=1;
-            }
-            conditions.push(condition)
-        }
-        else{
-            if(condition.value==-1){
-                existing.value = -1;
-            }
-            else{
-                existing.value++;
-            }
-        }
-    }
 }
--- units.js
+++ units.js
@@ -1,36 +1,3 @@
-var infantry = {
-    name: "Infantry",
-    id: "inf",
-    values: [
-        {turn: 0, value: 2, condition: "artillery"},
-        {turn: 0, value: 1, condition: "artillery", invertCondition:true},
-        {turn: 1, value: 2}],
-    price: 3
-};
-
-var artillery = {
-    name: "Artillery",
-    id: "art",
-    values: [
-        {turn: 0, value: 2},
-        {turn: 1, value: 2}],
-    conditions: [{name: "artillery"}],
-    price: 4
-}
-
-var aa = {
-    name: "AA Gun",
-    id: "aa",
-    values: [
-        {turn: 0, value: 1}],
-    conditions:[{name:"aaFired",type:"permanent", value:-1}],
-    priority:[
-        {value:1,condition:"aaFired", invertCondition:true},
-        {value:-1,condition:"aaFired"}
-    ],
-    price: 5
-}
-
-var units = [infantry, artillery, aa];
+var units = ["inf","art","mec","arm","aa","fig","tac","bom"]
 
 module.exports = units;
